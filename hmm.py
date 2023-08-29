dbd1cea8a74448dfa3a23be4352209e205011db9
Raymond Deng <1107450@gmail.com>
2023-08-28 23:18:11 -0700

    add init

--- hmm.py
+++ hmm.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 class HiddenMarkovModel:
-	def __init__(self):
-		pass
+	def __init__(self, n_state, x_size):
+		self.n_state = n_state
+		self.x_size = x_size
+		self.start_prob = np_ones(n_state) * 1.0 / n_state
+		self.transmat_prob = np.ones((n_state, n_state) * 1.0 / n_state)
 
 	@classmethod
 	def initialize():
