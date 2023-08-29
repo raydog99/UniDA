4ca6f190235df6bf74775e6e0815e08e5f70ba5b
Raymond Deng <1107450@gmail.com>
2023-08-28 23:18:51 -0700

    add generate seq

--- hmm.py
+++ hmm.py
@@ -11,8 +11,28 @@ class HiddenMarkovModel:
 	def initialize():
 		pass
 
+	def generate_seq(self, seq_length):
+        X = np.zeros((seq_length, self.x_size))
+        Z = np.zeros(seq_length)
+        Z_pre = np.random.choice(self.n_state, 1, p=self.start_prob)
+        X[0] = self.generate_x(Z_pre)
+        Z[0] = Z_pre
+
+        for i in range(seq_length):
+            if i == 0: continue
+            # P(Zn+1)=P(Zn+1|Zn)P(Zn)
+            Z_next = np.random.choice(self.n_state, 1, p=self.transmat_prob[Z_pre,:][0])
+            Z_pre = Z_next
+            # P(Xn+1|Zn+1)
+            X[i] = self.generate_x(Z_pre)
+            Z[i] = Z_pre
+
+        return X,Z
+
 	def update():
 		pass
 
 	def train():
 		pass
+
+	
