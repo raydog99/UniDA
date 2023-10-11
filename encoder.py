30be780e01df58377169202a2932c4245d2feb57
Raymond Deng <1107450@gmail.com>
2023-10-10 23:34:24 -0700

    add encoder, feed forward

---
+++ encoder.py
@@ -0,0 +1,125 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+from typing import Tuple
+
+from .feed_forward import FeedForwardModule
+from .attention import MultiHeadedSelfAttentionModule
+from .convolution import (
+    EncoderConvModule,
+    Conv2dSubampling,
+)
+from .modules import (
+    ResidualConnectionModule,
+    Linear,
+)
+
+
+class EncoderBlock(nn.Module):
+    def __init__(
+            self,
+            encoder_dim: int = 512,
+            num_attention_heads: int = 8,
+            feed_forward_expansion_factor: int = 4,
+            conv_expansion_factor: int = 2,
+            feed_forward_dropout_p: float = 0.1,
+            attention_dropout_p: float = 0.1,
+            conv_dropout_p: float = 0.1,
+            conv_kernel_size: int = 31,
+            half_step_residual: bool = True,
+    ):
+        super(EncoderBlock, self).__init__()
+        if half_step_residual:
+            self.feed_forward_residual_factor = 0.5
+        else:
+            self.feed_forward_residual_factor = 1
+
+        self.sequential = nn.Sequential(
+            ResidualConnectionModule(
+                module=FeedForwardModule(
+                    encoder_dim=encoder_dim,
+                    expansion_factor=feed_forward_expansion_factor,
+                    dropout_p=feed_forward_dropout_p,
+                ),
+                module_factor=self.feed_forward_residual_factor,
+            ),
+            ResidualConnectionModule(
+                module=MultiHeadedSelfAttentionModule(
+                    d_model=encoder_dim,
+                    num_heads=num_attention_heads,
+                    dropout_p=attention_dropout_p,
+                ),
+            ),
+            ResidualConnectionModule(
+                module=EncoderConvModule(
+                    in_channels=encoder_dim,
+                    kernel_size=conv_kernel_size,
+                    expansion_factor=conv_expansion_factor,
+                    dropout_p=conv_dropout_p,
+                ),
+            ),
+            ResidualConnectionModule(
+                module=FeedForwardModule(
+                    encoder_dim=encoder_dim,
+                    expansion_factor=feed_forward_expansion_factor,
+                    dropout_p=feed_forward_dropout_p,
+                ),
+                module_factor=self.feed_forward_residual_factor,
+            ),
+            nn.LayerNorm(encoder_dim),
+        )
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        return self.sequential(inputs)
+
+
+class EncoderEncoder(nn.Module):
+    def __init__(
+            self,
+            input_dim: int = 80,
+            encoder_dim: int = 512,
+            num_layers: int = 17,
+            num_attention_heads: int = 8,
+            feed_forward_expansion_factor: int = 4,
+            conv_expansion_factor: int = 2,
+            input_dropout_p: float = 0.1,
+            feed_forward_dropout_p: float = 0.1,
+            attention_dropout_p: float = 0.1,
+            conv_dropout_p: float = 0.1,
+            conv_kernel_size: int = 31,
+            half_step_residual: bool = True,
+    ):
+        super(EncoderEncoder, self).__init__()
+        self.conv_subsample = Conv2dSubampling(in_channels=1, out_channels=encoder_dim)
+        self.input_projection = nn.Sequential(
+            Linear(encoder_dim * (((input_dim - 1) // 2 - 1) // 2), encoder_dim),
+            nn.Dropout(p=input_dropout_p),
+        )
+        self.layers = nn.ModuleList([EncoderBlock(
+            encoder_dim=encoder_dim,
+            num_attention_heads=num_attention_heads,
+            feed_forward_expansion_factor=feed_forward_expansion_factor,
+            conv_expansion_factor=conv_expansion_factor,
+            feed_forward_dropout_p=feed_forward_dropout_p,
+            attention_dropout_p=attention_dropout_p,
+            conv_dropout_p=conv_dropout_p,
+            conv_kernel_size=conv_kernel_size,
+            half_step_residual=half_step_residual,
+        ) for _ in range(num_layers)])
+
+    def count_parameters(self) -> int:
+        return sum([p.numel() for p in self.parameters()])
+
+    def update_dropout(self, dropout_p: float) -> None:
+        for name, child in self.named_children():
+            if isinstance(child, nn.Dropout):
+                child.p = dropout_p
+
+    def forward(self, inputs: Tensor, input_lengths: Tensor) -> Tuple[Tensor, Tensor]:
+        outputs, output_lengths = self.conv_subsample(inputs, input_lengths)
+        outputs = self.input_projection(outputs)
+
+        for layer in self.layers:
+            outputs = layer(outputs)
+
+        return outputs, output_lengths
